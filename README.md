fca70e5d340a8d533d2f668da09234d2049fb04d
Vivian Yu <146399020+v3yu@users.noreply.github.com>
2025-05-20 22:06:09 +0000

    1

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
+1) Within a Github action that runs whenever code is pushed. This way we can make sure we never push code that breaks our software and can immediately narrow down what bugs need to be fixed because they must be in our most recent push. If we only test locally we may not catch bugs that are caused by interactions between our current code and code from other feature branches that other team members are working on, for instance, and if we test only at the end we may have to search through many files and function calls to find where a bug originates, or multiple unrelated bugs may compound each other.
 
-
+2) No, I would write unit tests for that
 
 
 
--- __tests__/lab7.test.js
+++ __tests__/lab7.test.js
@@ -21,7 +21,7 @@ describe('Basic user flow for Website', () => {
   // Check to make sure that all 20 <product-item> elements have data in them
   // We use .skip() here because this test has a TODO that has not been completed yet.
   // Make sure to remove the .skip after you finish the TODO. 
-  it.skip('Make sure <product-item> elements are populated', async () => {
+  it('Make sure <product-item> elements are populated', async () => {
     console.log('Checking to make sure <product-item> elements are populated...');
 
     // Start as true, if any don't have data, swap to false
@@ -38,10 +38,12 @@ describe('Basic user flow for Website', () => {
     console.log(`Checking product item 1/${prodItemsData.length}`);
 
     // Make sure the title, price, and image are populated in the JSON
-    firstValue = prodItemsData[0];
-    if (firstValue.title.length == 0) { allArePopulated = false; }
-    if (firstValue.price.length == 0) { allArePopulated = false; }
-    if (firstValue.image.length == 0) { allArePopulated = false; }
+      for (let i = 0; i < prodItemsData.length; i++) {
+        const item = prodItemsData[i];
+        if (!item.title || item.title.length === 0) { allArePopulated = false; }
+        if (!item.price || item.price.length === 0) { allArePopulated = false; }
+        if (!item.image || item.image.length === 0) { allArePopulated = false; }
+      }
 
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
@@ -57,7 +59,7 @@ describe('Basic user flow for Website', () => {
 
   // Check to make sure that when you click "Add to Cart" on the first <product-item> that
   // the button swaps to "Remove from Cart"
-  it.skip('Clicking the "Add to Cart" button should change button text', async () => {
+  it('Clicking the "Add to Cart" button should change button text', async () => {
     console.log('Checking the "Add to Cart" button...');
 
     /**
@@ -68,6 +70,13 @@ describe('Basic user flow for Website', () => {
      * Once you have the innerText property, use innerText.jsonValue() to get the text value of it
      * Remember to remove the .skip from this it once you are finished writing this test.
      */
+    const prodItem = await page.$('product-item');
+    const shadowRoot = await prodItem.getProperty('shadowRoot');
+    const button = await shadowRoot.$('button');
+    await button.click();
+    const buttonText = await button.getProperty('innerText');
+    const buttonTextValue = await buttonText.jsonValue();
+    expect(buttonTextValue).toBe('Remove from Cart');
 
   }, 2500);
 
